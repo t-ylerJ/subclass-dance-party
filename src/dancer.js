4a6ba962f647ac57d19260b2b73ada306069fae9
Tyler Johnson <tylerj0937@gmail.com>
2024-05-07 16:00:55 -0700

    fully implement functionality for blinky dancer and dancer files

--- src/dancer.js
+++ src/dancer.js
@@ -4,8 +4,6 @@
 
 var Dancer = function(top, left, timeBetweenSteps) {
   this.timeBetweenSteps = timeBetweenSteps;
-  this.top = top;
-  this.left = left;
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
@@ -13,26 +11,33 @@ var Dancer = function(top, left, timeBetweenSteps) {
 
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  this.setPosition(this.top, this.left);
+  this.setPosition(top, left);
 
 };
 
 
 Dancer.prototype.step = function() {
-
+  var scope = this;
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, this.timeBetweenSteps);
+  setTimeout(function() {
+    scope.step();
+  },
+  this.timeBetweenSteps);
+
 };
+
+
 
 Dancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   //
+
   var styleSettings = {
     top: this.top,
     left: this.left
-  }
+  };
 
   this.$node.css(styleSettings);
 };
